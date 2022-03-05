483c536cf8c5797f927ce9fc6a8f4c756ed29256
anatolii <anatoliise@mail.ru>
2022-03-05 14:42:54 +0300

    task2

---
+++ seminar1task/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите первое число ");
+string strNoumberA = Console.ReadLine();
+int NoumberA = int.Parse(strNoumberA);
+
+Console.WriteLine("введите второе число ");
+string strNoumberB = Console.ReadLine();
+int NoumberB = int.Parse(strNoumberB);
+
+Console.WriteLine("введите третье число ");
+string strNoumberC = Console.ReadLine();
+int NoumberC = int.Parse(strNoumberC);
+int Max=NoumberA;
+
+Console.Write("max=");
+
+if(NoumberA > NoumberB) 
+{
+    Max = NoumberA;
+}
+if(NoumberB > Max)
+{
+    Max = NoumberB;
+}
+if(NoumberC > Max)
+{
+
+    Max = NoumberC;
+}
+Console.Write(Max);
