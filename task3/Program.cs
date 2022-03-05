4a0aeec7ea2f2b87512e7ce8c533c4999ca21b1d
anatolii <anatoliise@mail.ru>
2022-03-05 23:50:45 +0300

    task2/2

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите число");
+string imputN = Console.ReadLine();
+int N = int.Parse(imputN);
+while(N<1000)
+{
+    if(N>100)
+        {
+            Console.Write("третья цифра ->  " + N%10);
+            break;
+        }
+        else
+        {
+            Console.Write("нет последней цифры");
+            break;
+        }
+      
+}        
+if(N > 1000)
+{
+    Console.Write("третья цифра -> " + N/10/10%10);
+}
