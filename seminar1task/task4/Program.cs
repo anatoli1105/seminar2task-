03f7a5a453733437acfbd4eb65744139ec0fdd2f
anatolii <anatoliise@mail.ru>
2022-03-05 14:45:32 +0300

    task4

---
+++ seminar1task/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите  число ");
+string strN = Console.ReadLine();
+int N = int.Parse(strN);
+int A = 0;
+while(A<N)
+{
+    if(N%2==1)N=N-1;
+    A=A+2;
+    Console.Write(A + " ");
+}
+Console.WriteLine(" <-четные числа ");
+
