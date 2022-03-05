6201db38a4bfef237bcb438372ce93852be62c88
anatolii <anatoliise@mail.ru>
2022-03-05 20:57:18 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,6 +1,6 @@
 ﻿Random rnd = new Random();
 //string inputN = Console.ReadLine();
-int N = rnd.Next(100,1000);
+int N = rnd.Next(1,7);
 Console.WriteLine("случайное  число -  " + (N));
 Console.Write(N/100);
 Console.Write(N%10);
